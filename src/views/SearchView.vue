5c3257fd7d2b1a2bcc97713928376e91e3f79658
ArtTrofimov <tilian.trofimov@gmail.com>
2026-07-24 14:24:48 +0200

    Creates a Search page

---
+++ src/views/SearchView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="page-grid">
+    <div v-if="isLoading" class="skeleton" aria-hidden="true">
+      <div class="title-skeleton sk-shimmer" />
+      <div class="grid-skeleton">
+        <div v-for="i in 12" :key="i" class="card-skeleton">
+          <div class="card-skeleton__poster sk-shimmer" />
+          <div class="card-skeleton__title sk-shimmer" />
+          <div class="card-skeleton__meta sk-shimmer" />
+        </div>
+      </div>
+    </div>
+    <ShowGrid v-else title="Search Results" :shows="searchList" />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useShowsSearchStore } from '@/store/shows.search'
+import ShowGrid from '@/components/ShowGrid.vue'
+
+const { searchList, isLoading } = storeToRefs(useShowsSearchStore())
+</script>
