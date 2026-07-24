2289386c3f1abecc011fbb2e601f3153d7235f09
ArtTrofimov <tilian.trofimov@gmail.com>
2026-07-24 14:24:38 +0200

    Creates a Favorites page

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="page-grid">
+    <ShowGrid title="My List" :shows="favouritesShowList" />
+  </section>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import { useShowsStore } from '@/store/shows'
+import ShowGrid from '@/components/ShowGrid.vue'
+
+const { favouritesShowList } = storeToRefs(useShowsStore())
+</script>
