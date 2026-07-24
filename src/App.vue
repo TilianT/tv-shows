c1f5bfb1305ac0d164e42207b964d1678545d839
ArtTrofimov <tilian.trofimov@gmail.com>
2026-07-24 14:19:01 +0200

    Creates App entry point, main.ts and sets up a local env variables

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <AppHeader />
+
+  <main class="main">
+    <RouterView v-slot="{ Component, route }">
+      <KeepAlive>
+        <component :is="Component" v-if="route.meta?.keepAlive" />
+      </KeepAlive>
+
+      <component :is="Component" v-if="!route.meta?.keepAlive" :key="route.fullPath" />
+    </RouterView>
+  </main>
+</template>
+
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader/AppHeader.vue'
+</script>
+
+<style scoped lang="scss">
+.main {
+  padding: 24px 48px;
+  padding-top: 0;
+}
+
+@media (max-width: 700px) {
+  .main {
+    padding: 16px;
+  }
+}
+</style>
